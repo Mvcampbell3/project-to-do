e8ea430a97242dd6500bc7bad823fe3c4efbc400
Mvcampbell3 <mvcampbell3@gmail.com>
2019-08-17 21:40:14 -0400

    added in more styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 const formTask = document.getElementById("formTask");
 const outputArea = document.getElementById("outputArea");
+const projectSelect = document.getElementById("projectSelect")
 
 const testTasks = [
   { group: "Project X", task: "Finish this part", completed: false },
@@ -15,7 +16,7 @@ function getTasks() {
   outputArea.innerHTML = "";
   const taskList = JSON.parse(localStorage.getItem("tasks-projects-to-do"));
 
-  if (taskList === null || taskList.length === 0) {
+  if (taskList === null) {
     outputArea.style.display = "none";
     return console.log("There is no task list");
   } else {
@@ -31,6 +32,19 @@ function getTasks() {
     orderedTasks.push(sameGroups);
   }
 
+  projectSelect.innerHTML = "";
+
+  const startOption = document.createElement("option");
+  startOption.value = "default";
+  startOption.textContent = "Pick Existing";
+  projectSelect.append(startOption);
+
+  taskGroupsDuplesss.forEach((one, i) => {
+    const optionPlace = document.createElement("option");
+    optionPlace.value = taskGroupsDuplesss[i];
+    optionPlace.textContent = taskGroupsDuplesss[i];
+    projectSelect.append(optionPlace)
+  })
   tasksToPage(orderedTasks, taskList);
 }
 
@@ -55,7 +69,10 @@ function tasksToPage(orderedTasks, taskList) {
         .then(result => getTasks())
         .catch(err => console.log(err));
     })
-    newGroup.append(newTitle, newGroupDelete);
+    const topHolder = document.createElement("div");
+    topHolder.classList = "topHolder";
+    topHolder.append(newTitle, newGroupDelete)
+    newGroup.append(topHolder);
 
     // create task div for each task
     one.forEach(task => {
@@ -79,7 +96,7 @@ function tasksToPage(orderedTasks, taskList) {
       })
       const delButton = document.createElement("button");
       delButton.classList = "taskBtn delBtn";
-      delButton.innerHTML = '<i class="awe far fa-window-close"></i>';
+      delButton.innerHTML = '<i class="awe fas fa-window-close"></i>';
       delButton.addEventListener("click", function() {
         const rightTask = taskList.filter(original => original.task === task.task);
         taskList.splice(taskList.indexOf(rightTask[0]), 1);
@@ -101,17 +118,43 @@ formTask.addEventListener("submit", function(e) {
   e.preventDefault();
   const taskType = document.getElementById("typeInput");
   const taskContent = document.getElementById("contentInput");
-  if (taskType.value.trim() === "" || taskContent.value.trim() === "") {
-    return alert("Not enough info dude")
+  const taskSelect = projectSelect.value;
+  console.log(taskSelect)
+  let sendObj;
+  if (taskSelect === "default") {
+    // taskType & taskContent
+    if (taskType.value.trim() === "" || taskContent.value.trim() === "") {
+      return alert("Not enough info, tasktype taskcontent")
+    } else {
+      // make object and return function call
+      // or set to local var and then call func after if else
+      sendObj = {group: taskType.value.trim(), task: taskContent.value.trim(), completed: false};
+    }
+  } else {
+    // taskSelect & taskContent
+    if (taskContent === "") {
+      return alert("Not enough info, taskContent, taskSelect")
+    } else {
+      sendObj = {group: taskSelect, task: taskContent.value.trim(), completed: false}
+    }
   }
+  updateTaskList(sendObj)
+  
+})
+
+function updateTaskList(task) {
   const taskList = JSON.parse(localStorage.getItem("tasks-projects-to-do"));
-  taskList.push({ group: taskType.value, task: taskContent.value, completed: false });
+  const taskType = document.getElementById("typeInput");
+  const taskContent = document.getElementById("contentInput");
+  // task needs to be object, group: groupName, task: task content, completed: false
+  taskList.push(task);
   Promise.all([setTasks(taskList)])
     .then(result => {
+      taskType.value = "";
       taskContent.value = "";
       getTasks()
     })
     .catch(err => console.log(err));
-})
+} 
 
 // setTasks(testTasks)
